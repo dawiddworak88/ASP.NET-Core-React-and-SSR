9f1aaf3cf89dea853e7d5ffe4d27113e6ded5b91
Dawid Dworak <dawiddworak88@gmail.com>
2020-01-31 17:28:21 +0100

    Added passing props to hydrate via window.data

--- fe/aspnetcore-react-ssr/src/pages/App/index.js
+++ fe/aspnetcore-react-ssr/src/pages/App/index.js
@@ -2,4 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-ReactDOM.hydrate(<App />, document.getElementById('root'));
+ReactDOM.hydrate(<App {...window.data} />, document.getElementById('root'));
